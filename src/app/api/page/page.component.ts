3e0e15f64e190dd233df770ffc6e8a8a457cfe63
Lucien Knechtli <lknechtli@azavea.com>
2017-03-16 17:33:53 -0400

    Use swagger-parser library
    Fetches yml from assets

--- src/app/api/page/page.component.ts
+++ src/app/api/page/page.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { SwaggerService } from '../swagger.service';
 
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
-  styleUrls: ['./page.component.scss']
+  styleUrls: ['./page.component.scss'],
+  providers: [SwaggerService],
 })
 export class ApiPageComponent implements OnInit {
-
-  constructor() { }
+  spec: any;
+  paths: Array<any>;
+  constructor(private swaggerService: SwaggerService) { }
 
   ngOnInit() {
+    this.swaggerService.fetchSpec().then((spec) => {
+      console.log(spec);
+      this.spec = spec;
+      this.paths = Object.keys(this.spec.paths);
+    }, (err) => {
+      console.error(err);
+    })
   }
 
 }
--- src/app/api/swagger.service.ts
+++ src/app/api/swagger.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+declare var SwaggerParser:any;
 
 @Injectable()
 export class SwaggerService {
 
   constructor() { }
 
+  public fetchSpec(): Promise<any> {
+    return new Promise((resolve, reject) => {
+      SwaggerParser.parse('/assets/spec.yml').then((file: Object) => {
+        resolve(file);
+      }, (err) => {
+        reject(err)
+      });
+    })
+  }
 }
